324e91c3dc66714df32e381bc9c8bd15e24ab6ae
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-09-12 21:20:30 -0400

    updated connection credentials to link to JAWSDF

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "hnroot",
-    database: "burgers_db"
+    user: "twnmfb5oapbyvf69",
+    password: "nc3sjm9rqd2gbfb0",
+    database: "hatiw45uc42nzf44"
 });
 }
 
